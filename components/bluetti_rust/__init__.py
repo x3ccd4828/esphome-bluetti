8fe6817e23084a9ce92be53f08da1ca82ade7f68
ESPHome User <esphome@local>
2026-02-07 23:46:31 -0800

    fix: use cg.add_library for proper linking

--- components/bluetti_rust/__init__.py
+++ components/bluetti_rust/__init__.py
@@ -32,5 +32,4 @@ async def to_code(config):
     component_dir = os.path.dirname(os.path.realpath(__file__))
     lib_dir = os.path.join(component_dir, "lib")
     
-    cg.add_build_flag(f"-L{lib_dir}")
-    cg.add_build_flag("-lbluetti_encryption")
+    cg.add_library("bluetti_encryption", lib_dir)
