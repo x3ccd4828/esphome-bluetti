87863768ace75799f7dc80d03e32521de5a92f3d
ESPHome User <esphome@local>
2026-02-15 17:20:45 -0800

    fix: enable mbedtls MD5 and link script for bluetti

--- components/bluetti_rust/__init__.py
+++ components/bluetti_rust/__init__.py
@@ -25,3 +25,8 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
+    
+    # Add custom build script to link mbedtls properly
+    import pathlib
+    script_path = pathlib.Path(__file__).parent / "link_mbedtls.py"
+    cg.add_platformio_option("extra_scripts", ["pre:" + str(script_path)])
